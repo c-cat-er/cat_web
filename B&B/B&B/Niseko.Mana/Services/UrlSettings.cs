782ea9d06369570ff23b0f5e4b0de66dded6f039
c-cat-er <129288174+c-cat-er@users.noreply.github.com>
2024-12-12 13:13:21 +0800

    Delete B&B/B&B/Niseko.Mana directory

--- B&B/B&B/Niseko.Mana/Services/UrlSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Niseko.Mana.Services
-{
-    public class UrlSettings
-    {
-        public string HttpUrl { get; set; }
-        public string HttpsUrl { get; set; }
-        public string IISExpressUrl { get; set; }
-    }
-}
